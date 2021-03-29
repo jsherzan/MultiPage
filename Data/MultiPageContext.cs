cf443584eed1b56d90ad50b08da527d3c2985317
jsherzan <77797866+jsherzan@users.noreply.github.com>
2021-03-28 23:59:43 -0500

    Add files via upload

---
+++ Data/MultiPageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MultiPage.Models;
+
+namespace MultiPage.Data
+{
+    public class MultiPageContext : DbContext
+    {
+        public MultiPageContext (DbContextOptions<MultiPageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MultiPage.Models.Contact> Contacts { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiPage.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string StateAbr { get; set; }
+        public string ZipCode { get; set; }
+        public string Notes { get; set; }
+    }
+}
